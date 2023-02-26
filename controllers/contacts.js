67fdf467e9861a6c2fdeebd8ad9e39ab2a91a6c6
Vlad Krasovsky <v.o.krasovsky@gmail.com>
2023-02-26 09:39:46 +0200

    Replaces json model with mongoose

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,15 +1,15 @@
-const contacts = require('../models/contacts')
+const { Contact } = require('../models/contact')
 
 const { HttpError, ctrlWrapper } = require('../helpers')
 
 const getAll = async (req, res) => {
-  const result = await contacts.listContacts()
+  const result = await Contact.find()
   res.json(result)
 }
 
 const getById = async (req, res) => {
-  const { contactId } = req.params
-  const result = await contacts.getContactById(contactId)
+  const { id } = req.params
+  const result = await Contact.findById(id)
   if (!result) {
     throw HttpError(404, 'Not found')
   }
@@ -17,13 +17,22 @@ const getById = async (req, res) => {
 }
 
 const add = async (req, res) => {
-  const result = await contacts.addContact(req.body)
+  const result = await Contact.create(req.body)
   res.status(201).json(result)
 }
 
 const updateById = async (req, res) => {
-  const { contactId } = req.params
-  const result = await contacts.updateContact(contactId, req.body)
+  const { id } = req.params
+  const result = await Contact.findByIdAndUpdate(id, req.body, { new: true })
+  if (!result) {
+    throw HttpError(404, 'Not found')
+  }
+  res.json(result)
+}
+
+const updateFavorite = async (req, res) => {
+  const { id } = req.params
+  const result = await Contact.findByIdAndUpdate(id, req.body, { new: true })
   if (!result) {
     throw HttpError(404, 'Not found')
   }
@@ -31,8 +40,8 @@ const updateById = async (req, res) => {
 }
 
 const deleteById = async (req, res) => {
-  const { contactId } = req.params
-  const result = await contacts.removeContact(contactId)
+  const { id } = req.params
+  const result = await Contact.findByIdAndRemove(id)
   if (!result) {
     throw HttpError(404, 'Not found')
   }
@@ -44,5 +53,6 @@ module.exports = {
   getById: ctrlWrapper(getById),
   add: ctrlWrapper(add),
   updateById: ctrlWrapper(updateById),
+  updateFavorite: ctrlWrapper(updateFavorite),
   deleteById: ctrlWrapper(deleteById),
 }
