11e51ca89917eee001aec51ff625ee6a63fb3431
Vlad Krasovsky <v.o.krasovsky@gmail.com>
2023-03-05 15:27:49 +0200

    Updates pagination magic numbers with variables

--- middlewares/pagination.js
+++ middlewares/pagination.js
@@ -1,10 +1,12 @@
 const pagination = async (req, res, next) => {
+  const { PER_PAGE_LIMIT_DEFAUL = 20, PER_PAGE_LIMIT_MAX = 50 } = process.env
+
   // const { page = 1, limit = 20 } = req.query
   const page = parseInt(req.query.page) || 1
-  let limit = parseInt(req.query.limit) || 20
+  let limit = parseInt(req.query.limit) || PER_PAGE_LIMIT_DEFAUL
 
-  if (limit > 50) {
-    limit = 50
+  if (limit > PER_PAGE_LIMIT_MAX) {
+    limit = PER_PAGE_LIMIT_MAX
   }
 
   const skip = (page - 1) * limit
