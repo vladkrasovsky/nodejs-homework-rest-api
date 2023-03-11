67828b027cc6856523a7f4b629329ae9bca93ccc
Vlad Krasovsky <v.o.krasovsky@gmail.com>
2023-03-11 03:13:23 +0200

    Adds upload middleware

--- middlewares/index.js
+++ middlewares/index.js
@@ -2,10 +2,12 @@ const authenticate = require('./authenticate')
 const validateBody = require('./validateBody')
 const isValidId = require('./isValidId')
 const pagination = require('./pagination')
+const upload = require('./upload')
 
 module.exports = {
   authenticate,
   validateBody,
   isValidId,
   pagination,
+  upload,
 }
